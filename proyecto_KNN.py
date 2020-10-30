116762c03f8a955f69c0b0d55c19a4288eadf6f3
Paola-Tirado <73606018+Paola-Tirado@users.noreply.github.com>
2020-10-29 20:33:49 -0600

    Función calc_distances()

--- proyecto_KNN.py
+++ proyecto_KNN.py
@@ -6,6 +6,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from statistics import mode
 
+def calc_distances(x0, y0, X):
+    # genera un arreglo con las distancias entre un punto y todos los puntos de X
+    distances = []
+    for i in range(X.shape[0]):
+        point = X[i, 0:2]
+        distances.append(euclidean_distance(x0, y0, point[0], point[1]))
+    return np.array(distances)
+
 def clasificacion_knn(X, y, px, py, k=False):
     # devuelve la clase a la que pertenece el punto
     if not k:
