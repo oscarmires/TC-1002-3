edf6f495ec79b2723c3ab9fbf19340e4db6cc48d
saalej <64176067+saalej@users.noreply.github.com>
2020-10-29 20:52:58 -0700

    Update proyecto_KNN.py

--- proyecto_KNN.py
+++ proyecto_KNN.py
@@ -1,13 +1,35 @@
-# Proyecto: implementación de algoritmo KNN
+"""
+Proyecto: implementación de algoritmo KNN
+
+Autores:   Oscar Miranda Escalante A01630791 (oscarmires)
+           Rodrigo Morales Aguayo A01632834 (ROmorales08)
+           Ana Paola Tirado Gonzalez (Paola-Tirado)
+           Roberto López Cisneros A01637335 (RobertMex)
+           Marian Alejandra Herrera Ayala A00227534 (saalej)
+Ultima fecha actualizacion: 29 Octubre 2020
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from statistics import mode
 
+#Roberto López Cisneros A01637335 (RobertMex)
+# Grafica los puntos y los puntos proyectados
+def graphPoints(X, y):  
+    for i in range(X.shape[0]):
+        p = X[i, 0:2]
+        if y[i] == 0:
+            plt.scatter(p[0], p[1], color="red", s=4)
+        elif y[i] == 1:
+            plt.scatter(p[0], p[1], color="black", s=4)
+        elif y[i] == 2:
+            plt.scatter(p[0], p[1], color="blue", s=4)
+    plt.show()
+
 # Oscar Miranda Escalante A01630791 (oscarmires)
+# devuelve la clase a la que pertenece el punto
 def clasificacion_knn(X, y, px, py, k=False):
-    # Devuelve la clase a la que pertenece el punto
     if not k:
         k = int(len(X) ** 0.5)
     nearest_neighbors = calc_distances(px, py, X).argsort()[0:k]
@@ -21,18 +43,19 @@ def clasificacion_knn(X, y, px, py, k=False):
     return mode_knn
 
 # Rodrigo Morales Aguayo A01632834 (ROmorales08)
+# genera un arreglo con las distancias entre un punto y todos los puntos de X
 def calc_distances(x0, y0, X):
-    # Genera un arreglo con las distancias entre un punto y todos los puntos de X
     distances = []
     for i in range(X.shape[0]):
         point = X[i, 0:2]
         distances.append(euclidean_distance(x0, y0, point[0], point[1]))
     return np.array(distances)
 
-# Marian Alejandra Herrera Ayala (saalej)
-df = pd.read_csv("iris1.csv") # Importar datos
+# Marian Alejandra Herrera Ayala A00227534 (saalej)
+# importar datos
+df = pd.read_csv("iris1.csv")
 
-# Adecuar datos
+# adecuar datos
 clases = ["Iris-versicolor", "Iris-virginica", "Iris-setosa"]
 df["Tipo_Flor"] = df["Tipo_Flor"].replace(clases, [0, 1, 2])
 data = df.values
@@ -41,9 +64,7 @@ y = data[:, -1]
 y = y.astype('int32')
 
 #Ana Paola Tirado Gonzalez (Paola-Tirado)
-# Nuevo punto para clasificar
+# nuevo punto para clasificar
 nuevo_punto = [5.0, 3.3]
 clase_resultado = clasificacion_knn(X, y, nuevo_punto[0], nuevo_punto[1])
 print("Tipo de flor del nuevo dato:", clases[clase_resultado])
-
-
