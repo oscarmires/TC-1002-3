02d4620b21acc187839200ba7013e663f3f4cd6d
Oscar Miranda <43005150+oscarmires@users.noreply.github.com>
2020-10-29 20:09:34 -0600

    Se agregó la función clasificacion_knn()

--- proyecto_KNN.py
+++ proyecto_KNN.py
@@ -1,6 +1,21 @@
 # Proyecto: implementación de algoritmo KNN
+# Oscar Miranda Escalante A01630791 (oscarmires)
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from statistics import mode
+
+def clasificacion_knn(X, y, px, py, k=False):
+    # devuelve la clase a la que pertenece el punto
+    if not k:
+        k = int(len(X) ** 0.5)
+    nearest_neighbors = calc_distances(px, py, X).argsort()[0:k]
+    mode_knn = mode(y[nearest_neighbors])
+    print("Nearest neighbors' indexes:", nearest_neighbors)
+    print("Moda: ", mode_knn)
+    plt.scatter(px, py, color="black", marker='^', s=30)
+    for i in range(k):
+        plt.scatter(X[nearest_neighbors[i], 0], X[nearest_neighbors[i], 1], color="yellow", s=20)
+    graphPoints(X, y)
+    return mode_knn
